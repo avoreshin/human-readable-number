7ced5d217d3a6fa28fff287e734d8422f823ba0e
avoreshin <avoreshin@gmail.com>
2022-01-05 21:32:41 +0300

    fix: function toReadable

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 module.exports = function toReadable(num) {
     let result;
-    let part1 = ["zero", "one", "two", "tree", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+    let part1 = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
     let part2 = ["","twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
     let remainder;
     result ='';
 
     if  (num%100 === 0 && num >= 100) {
-        result = (part1[Math.trunc(num / 100)] + " hundred ");
+        result = (part1[Math.trunc(num / 100)] + " hundred");
         return result;
     }
     if  ((num >= 100 && num < 1000) && (num%100 !== 0)){
-        result = (part1[Math.trunc(num/100)]+" hundred ");
+        result = (part1[Math.trunc(num/100)]+" hundred");
         remainder = (num%100);
     }
-    if ((remainder && remainder/10 > 0) || (!remainder && num/10 > 0 && num > 19)){
+    if ((remainder && remainder > 19) || (!remainder && num/10 > 0 && num > 19)){
         if (!remainder)
             remainder = num;
+        else
+        result += " ";
         result += (part2[Math.trunc(remainder/10)-1]);
         remainder = (num%10);
     }
@@ -26,5 +28,6 @@ module.exports = function toReadable(num) {
             result += " ";
         result += (part1[remainder]);
     }
-    return(result);
+    return result;
 }
+
